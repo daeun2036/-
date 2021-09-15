efa5d211b626c0504d88030fdbefc7933922a408
daeun2036 <moderato2036@naver.com>
2021-09-16 07:47:43 +0900

    .

--- Beginning/exec.js
+++ Beginning/exec.js
@@ -2,6 +2,7 @@ const exec = require('child_process').exec;
 
 var process = exec('ls');
 
+// 성공적인 결과는 stdout에서 실패한 결과는 stdderr에서 버퍼형태로 전달
 process.stdout.on('data',function(data){
     console.log(data.toString());
 }); // 실행 결과
--- Beginning/worker_threads.js
+++ Beginning/worker_threads.js
@@ -1,10 +1,10 @@
 const { 
     Worker, isMainThread, parentPort,
-  } = require('worker_threads'); 
+  } = require('worker_threads'); // worker_threads 모듈
 
 if(isMainThread){ // 부모일 때
-    const
-    worker = new Worker(__filename);
+    // new Worker를 통해 현재 파일을 워커 스레드에서 실행
+    const worker = new Worker(__filename);
     worker.on('message',message => console.log('from worker',message));
     worker.on('exit',() => console.log('worker exit'));
     worker.postMessage('ping');
